134b77546b291ee240a49761d4f986a4b545e0d7
Shikhar Rastogi <shikhar.rastogi1@gmail.com>
2022-05-24 14:58:18 -0500

    With Solidity 0.8.9

---
+++ deploy.js
@@ -0,0 +1,26 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+
+const { abi, evm } = require('./compile');
+
+provider = new HDWalletProvider(
+    'air wool judge anchor bamboo space system recall pyramid supreme inject clinic',
+    'https://rinkeby.infura.io/v3/217469ddd7b6444fb83870cb6828c118'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    const result = await new web3.eth.Contract(abi)
+        .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+        .send({ gas: '1000000', from: accounts[0] });
+
+    console.log('Contract deployed to', result.options.address);
+    provider.engine.stop();
+};
+
+deploy();
